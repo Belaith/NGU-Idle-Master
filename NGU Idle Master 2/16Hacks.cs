7a32515e372c15710ba22e641f9661c6810a0093
Belaith <Quanti_T@web.de>
2019-05-08 20:14:19 +0200

    Changed Hacklogic ("Advance Dust" works different from other "Advance"-Options...)

--- NGU Idle Master 2/16Hacks.cs
+++ NGU Idle Master 2/16Hacks.cs
@@ -28,10 +28,64 @@ namespace NGU_Idle_Master
             Y = 503
         };
 
+        public static readonly Point pointPageHacks1 = new Point()
+        {
+            X = 353,
+            Y = 192
+        };
+
+        public static readonly Point pointPageHacks2 = new Point()
+        {
+            X = 413,
+            Y = 192
+        };
+
         public static readonly Point pointHack1 = new Point()
         {
             X = 574,
             Y = 243
+        };
+
+        public static readonly Point pointHack2 = new Point()
+        {
+            X = 899,
+            Y = 243
+        };
+
+        public static readonly Point pointHack3 = new Point()
+        {
+            X = 574,
+            Y = 336
+        };
+
+        public static readonly Point pointHack4 = new Point()
+        {
+            X = 899,
+            Y = 336
+        };
+
+        public static readonly Point pointHack5 = new Point()
+        {
+            X = 574,
+            Y = 429
+        };
+
+        public static readonly Point pointHack6 = new Point()
+        {
+            X = 899,
+            Y = 429
+        };
+
+        public static readonly Point pointHack7 = new Point()
+        {
+            X = 574,
+            Y = 522
+        };
+
+        public static readonly Point pointHack8 = new Point()
+        {
+            X = 899,
+            Y = 522
         };
 
         #endregion
@@ -52,7 +106,25 @@ namespace NGU_Idle_Master
 
             window.Click(HacksConstants.pointPageHacks, false, true);
 
+            window.Click(HacksConstants.pointPageHacks1, false, true);
             window.Click(HacksConstants.pointHack1, false, false);
+            window.Click(HacksConstants.pointHack2, false, false);
+            window.Click(HacksConstants.pointHack3, false, false);
+            window.Click(HacksConstants.pointHack4, false, false);
+            window.Click(HacksConstants.pointHack5, false, false);
+            window.Click(HacksConstants.pointHack6, false, false);
+            window.Click(HacksConstants.pointHack7, false, false);
+            window.Click(HacksConstants.pointHack8, false, false);
+
+            window.Click(HacksConstants.pointPageHacks2, false, true);
+            window.Click(HacksConstants.pointHack1, false, false);
+            window.Click(HacksConstants.pointHack2, false, false);
+            window.Click(HacksConstants.pointHack3, false, false);
+            window.Click(HacksConstants.pointHack4, false, false);
+            window.Click(HacksConstants.pointHack5, false, false);
+            window.Click(HacksConstants.pointHack6, false, false);
+            window.Click(HacksConstants.pointHack7, false, false);
+            window.Click(HacksConstants.pointHack8, false, false);
         }
     }
 }
